8ee042175a9acf603f08274e84e105edde95cf68
MrSabin <sabin.21011986@gmail.com>
2022-05-19 21:27:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+import datetime
+
+import pandas
+
+excel_data_df = pandas.read_excel("wine.xlsx")
+wines_list = excel_data_df.to_dict('records')
+
+begin = datetime.datetime(year=1920, month=1, day=1)
+now = datetime.date.today()
+delta = now.year - begin.year
+
+if (delta%10 == 1) and (delta != 11) and (delta != 111):
+   year_word = "год"
+elif (delta%10 > 1) and (delta%10 < 5) and (delta != 12) and (delta != 13) and (delta != 14):
+   year_word = "года"
+else:
+   year_word = "лет"
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(
+    age = delta,
+    wines = wines_list,
+    word = year_word
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+#server.serve_forever()
